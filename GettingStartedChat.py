d34bb94bca166c2d0dead7af79decbaaeb0afd9a
spoullados <92266030+spoullados@users.noreply.github.com>
2024-09-03 15:15:10 +0200

    Update GettingStartedChat.py

--- GettingStartedChat.py
+++ GettingStartedChat.py
@@ -74,7 +74,6 @@ def generate_response(openai_client: OpenAI, model: str, messages: list):
 if __name__ == "__main__":
     try:
         asyncio.run(chat())
-        print("hellowssss")
     except KeyboardInterrupt:
         pass
 
