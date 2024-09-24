18520eeff041c593653beb35e76cfb511456fa6a
Jonas Länzlinger <laenzlinger.jonas@gmail.com>
2024-09-24 15:27:29 +0200

    test

--- git-switch-config.sh
+++ git-switch-config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 if [ "$1" == "JONAS_LAENZLINGER" ]; then
-   git config user.name "JONAS_LAENZLINGER"
+   git config user.name "Jonas Länzlinger"
    git config user.email "laenzlinger.jonas@gmail.com"
 else
    echo "Usage: $0 {SURNAME_NAME_1|SURNAME_NAME_2}"
