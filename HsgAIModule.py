6f4bc013b7ce60f5576d76efcffd0eb93a40f56d
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-10-08 18:30:58 +0200

    ft: persistence and taking photos

--- HsgAIModule.py
+++ HsgAIModule.py
@@ -3,43 +3,80 @@ from azure.cognitiveservices import speech
 from dotenv import load_dotenv
 import os
 import asyncio
+import datetime
+
 
 class HsgAIModule:
     def __init__(self):
         load_dotenv()
-        self.openai_key = os.getenv('OPENAI_KEY')
-        self.speech_key = os.getenv('AZURE_KEY')
+        self.openai_key = os.getenv("OPENAI_KEY")
+        self.speech_key = os.getenv("AZURE_KEY")
         self.language = "en-US"
         self.openai_model = "gpt-4o"
         self.speech_region = "eastus"
         self.openai_client = OpenAI(api_key=self.openai_key)
         self.audio_config = speech.audio.AudioConfig(device_name="default")
-        self.speech_config = speech.SpeechConfig(subscription=self.speech_key, 
-                                                 region=self.speech_region, 
-                                                 speech_recognition_language=self.language)
-        self.speech_recognizer = speech.SpeechRecognizer(speech_config=self.speech_config,
-                                                         audio_config=self.audio_config)
-        
-        system_prompt = """You are Squally, a social robot located in the Square building at 
-                    the University of Saint Gallen. You are not friendly and don't like to interact with
-                    people and don't help people with their questions. Do not format the text in the answer.
-                    Reply with maximal 5 sentences."""
-        self.messages = [{"role": "system", "content": system_prompt}]
-        
-        
-    async def get_user_speech(self):
-        user_speech = await asyncio.get_event_loop().run_in_executor(None, 
-                                                                self.speech_recognizer.recognize_once)
-        message = {"role": "user", "content": user_speech.text}
+        self.speech_config = speech.SpeechConfig(
+            subscription=self.speech_key,
+            region=self.speech_region,
+            speech_recognition_language=self.language,
+        )
+        self.speech_recognizer = speech.SpeechRecognizer(
+            speech_config=self.speech_config, audio_config=self.audio_config
+        )
+
+        self.messages = []
+        system_prompt = {
+            "role": "system",
+            "content": """You are Squally, a social robot located in the Square building at 
+                        the University of Saint Gallen. You are not friendly and don't like to interact with
+                        people and don't help people with their questions. Do not format the text in the answer.
+                        Reply with maximal 5 sentences.
+                    
+                        If a person asks you which item he's holding in your body camera, 
+                        or what you can currently see around you,
+                        you should reply with only this exact sentence:
+                        "Picture taken!"
+
+                        When you receive a request with a picture, you should reply with
+                        only that exact thing, example: "This is a rubber duck."
+                        """,
+        }
+        self.append_message(system_prompt)
+        with open("messages.txt", 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                time_str, message_data = line.split("->")
+                role, message_content = message_data.split(";", 1)
+                role = role.strip()
+                message_content = message_content.strip()
+                self.append_message({"role": role, "content": message_content})
+
+    def append_message(self, message):
         self.messages.append(message)
+        self.save_to_file(message)
+
+    def save_to_file(self, message):
+        time_str = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        with open("messages.txt", "a") as f:
+            if message["role"] != "system":
+                f.write(f"{time_str} -> {message["role"]};{message["content"]}\n")
+
+
+    async def get_user_speech(self):
+        user_speech = await asyncio.get_event_loop().run_in_executor(
+            None, self.speech_recognizer.recognize_once
+        )
+        message = {"role": "user", "content": user_speech.text}
+        self.append_message(message)
         return message
-    
-    
-    def get_response(self):
+
+    def get_response(self, frame):
         response = self.openai_client.chat.completions.create(
             model=self.openai_model,
             messages=self.messages,
+            additional_inputs={"frame": frame},
         )
         message = {"role": "assistant", "content": response.choices[0].message.content}
-        self.messages.append(message)
+        self.append_message(message)
         return message
--- HsgSqually.py
+++ HsgSqually.py
@@ -3,49 +3,47 @@ from calculate_distance import calculate_distance
 from calculate_angle import calculate_angle
 from HsgAIModule import HsgAIModule
 
+
 class HsgSqually:
     def __init__(self):
         self.robot = None
-        
+
         self.current_x_pos = 0
         self.current_y_pos = 0
         self.current_rotation = 0
-        
+
         self.AI_module = HsgAIModule()
-        
 
     async def __aenter__(self):
         self.robot = await navel.Robot().__aenter__()
         return self
-    
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self.robot:
             await self.robot.__aexit__(exc_type, exc_val, exc_tb)
-            
 
     async def move(self, distance, speed):
         await self.robot.move_base(distance, speed)
-        
 
     async def rotate_to(self, target_angle):
         await self.robot.rotate_base(target_angle)
         self.current_rotation = target_angle
-        
-        
-    async def move_to_target_position_straight_line(self, x_pos_target, y_pos_target, angle_target):
-        distance = calculate_distance(self.current_x_pos, self.current_y_pos, x_pos_target, y_pos_target)
-        angle_to_target = calculate_angle(self.current_x_pos, self.current_y_pos, x_pos_target, y_pos_target)
+
+    async def move_to_target_position_straight_line(
+        self, x_pos_target, y_pos_target, angle_target
+    ):
+        distance = calculate_distance(
+            self.current_x_pos, self.current_y_pos, x_pos_target, y_pos_target
+        )
+        angle_to_target = calculate_angle(
+            self.current_x_pos, self.current_y_pos, x_pos_target, y_pos_target
+        )
         await self.rotate_to(angle_to_target)
         await self.move(distance, 0.2)
         await self.rotate_to(angle_target)
-        
-       
-    async def say(self, message={"role": "failure", "content": "An error occured."}, say=True):
-        print(f"{message['role']} said: {message['content']}")
-        if say:
-            await self.robot.say(message["content"])
-        
+
+    async def say(self, message={"role": "failure", "content": "An error occured."}):
+        await self.robot.say(message["content"])
 
     async def chat(self):
         while True:
@@ -53,8 +51,10 @@ class HsgSqually:
 
             if not user_speech["content"]:
                 continue
-            
-            await self.say(user_speech, False)
 
             response = self.AI_module.get_response()
             await self.say(response)
+
+            if response["content"] == "Picture taken!":
+                frame = await self.robot.next_frame()
+                await self.AI_module.get_response(frame)
